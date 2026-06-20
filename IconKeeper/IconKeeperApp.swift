d25143606dcd304540bc8ea92788a63bd9261915
developer180527 <venugopal9836n@gmail.com>
2026-06-20 13:26:48 +0530

    Initial Commit

---
+++ IconKeeper/IconKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconKeeperApp.swift
+//  IconKeeper
+//
+//  Created by Venu Gopal on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct IconKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
